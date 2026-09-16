7f60e3310f98e53063624af749862dd50dadfaf6
migo <tadoyjewel71@gmail.com>
2026-09-16 18:42:16 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const printBtn =
+    document.getElementById("printBtn");
+
+
+  if (printBtn) {
+
+    printBtn.addEventListener(
+      "click",
+      function () {
+
+        window.print();
+
+      }
+    );
+
+  }
+
+});
